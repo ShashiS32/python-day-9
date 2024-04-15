5ac9014fcab21de19c369ec09d41095b51ddf709
Shashwat Singh <46977833+ShashiS32@users.noreply.github.com>
2024-04-15 22:46:42 +0000

    Dictionary

---
+++ main.py
@@ -0,0 +1,20 @@
+dictionary = {
+  "Bug" : "An error in a program that prevents the program from running as expected.",
+
+  "Function" : "A piece of code that you can easily call over and over again."
+
+  
+}
+
+
+
+# added_word = input("What is the new word you want to add: ")
+# added_def = input("What is the definition of the new word: ")
+
+# # dictionary[added_word] = added_def
+
+# # dictionary["Bug"] = "A moth in your computer"
+# print(dictionary["Bug"])
+
+for things in dictionary:
+  print(dictionary[things])
